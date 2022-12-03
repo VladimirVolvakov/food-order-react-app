d0ff48e3c8d5e1ddb499b845b849282e17c37b17
VladimirVolvakov <dr.volvakov@gmail.com>
2022-12-03 19:08:10 +0300

    feat: add markup & styles for Card component

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,12 @@
+// Styles:
+import { Container } from "./Card.styles";
+
+const Card = ({ children }) => {
+  return (
+    <Container>
+      { children }
+    </Container>
+  );
+};
+
+export default Card;
---
+++ src/components/UI/Card.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: white;
+  padding: 1rem;
+  border-radius: 1rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+`;
