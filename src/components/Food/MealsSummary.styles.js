8597feaf821a334385b16fe94ca80a6028d1920a
VladimirVolvakov <dr.volvakov@gmail.com>
2022-12-03 18:15:37 +0300

    style: add styles for MealsSummary component

--- src/components/Food/MealsSummary.styles.js
+++ src/components/Food/MealsSummary.styles.js
@@ -1,7 +1,26 @@
 import styled from "styled-components";
 
-export const Summary = styled.section``;
+export const Summary = styled.section`
+  background-color: #b28f53;
+  color: white;
+  text-align: center;
+  letter-spacing: 0.06em;
+  width: 90%;
+  max-width: 45rem;
+  position: relative;
+  margin: -10rem auto 0;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 0 1px 18px 10px rgba(178, 143, 83, 0.35);
+`;
 
-export const Title = styled.h2``;
+export const Title = styled.h2`
+  font-size: 2rem;
+`;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  margin-top: 1.25rem;
+  font-size: 1.25rem;
+  line-height: 1.4;
+  //text-align: justify;
+`;
