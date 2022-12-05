8b92c703c1289743c4e7098e31a71b09e9e7beb3
VladimirVolvakov <dr.volvakov@gmail.com>
2022-12-06 01:17:36 +0300

    feat: implement access of CartButton component to CartContext, add rendering number of cart items

--- src/components/Layout/CartButton.jsx
+++ src/components/Layout/CartButton.jsx
@@ -2,15 +2,27 @@
 import { Amount, Button, ButtonText, CartIconContainer } from "./CartButton.styles";
 // Icon:
 import { AiOutlineShoppingCart } from "react-icons/ai";
+// Hook:
+import { useContext } from "react";
+// Context:
+import CartContext from "../../store/cartContext";
 
 const CartButton = ({ onClick }) => {
+  const context = useContext(CartContext);
+
+  const quantityOfCartItems = context.items.reduce(
+    (numberOfItems, item) => {
+      return numberOfItems + item.amount;
+    }, 0
+  );
+
   return (
     <Button className="abcd">
       <CartIconContainer>
         <AiOutlineShoppingCart />
       </CartIconContainer>
       <ButtonText onClick={onClick}>Your Cart</ButtonText>
-      <Amount>4</Amount>
+      <Amount>{quantityOfCartItems}</Amount>
     </Button>
   );
 };
